d43307a4988aabb05749bb08e1ed4f13f7d9c1be
Plactrix <48103197+Plactrix@users.noreply.github.com>
2022-09-15 16:34:43 -0700

    Create sv_utils.lua
    Organized & Updated old natives

---
+++ fivem_script/tokovoip_script/server/sv_utils.lua
@@ -0,0 +1,28 @@
+-- Functions
+function tablelength(T)
+	local count = 0
+	for _ in pairs(T) do
+		count = count + 1
+	end
+	return count
+end
+
+local charset = {} do
+	for c = 48, 57 do 
+		table.insert(charset, string.char(c))
+	end
+	for c = 65, 90 do
+		table.insert(charset, string.char(c))
+	end
+	for c = 97, 122 do
+		table.insert(charset, string.char(c))
+	end
+end
+
+function randomString(length)
+	if not length or length <= 0 then
+		return ''
+	end
+	math.randomseed(os.time())
+	return randomString(length - 1) .. charset[math.random(1, #charset)]
+end
