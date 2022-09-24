f731b5f14bd44f49438333713f075d6f2118e3b5
Plactrix <48103197+Plactrix@users.noreply.github.com>
2022-09-23 18:01:10 -0700

    Moved audio listener to script.js

--- fivem_script/tokovoip_script/html/script.js
+++ fivem_script/tokovoip_script/html/script.js
@@ -200,18 +200,23 @@ function receivedClientCall (event) {
 		} else if (eventName == 'updateTokovoipInfo') {
 			if (connected)
 				updateTokovoipInfo(payload, 1);
-
 		} else if (eventName == 'updateTokoVoip') {
 			voip.plugin_data = payload;
 			updatePlugin();
-
 		} else if (eventName == 'disconnect') {
 			sendData('disconnect');
 			voipStatus = NOT_CONNECTED;
 		} else if (eventName == 'toggleLatency') {
 			displayLatency = !displayLatency;
 			document.querySelector('#latency').style.display = (displayLatency) ? 'block' : 'none';
-		}
+		} else if (eventName == 'playSound') {
+			if (audioPlayer != null) {
+				audioPlayer.pause();
+			}
+			audioPlayer = new Howl({src: ["./sounds/" + event.data.transactionFile + ".wav"]});
+			audioPlayer.volume(event.data.transactionVolume);
+			audioPlayer.play();
+        }
 	}
 
 	checkPluginStatus();
