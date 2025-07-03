f74ba73f1279fcdc1a78a34a353a999641bd236e
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2025-07-04 01:43:55 +0200

    Update fxmanifest.lua

--- fivem_script/tokovoip_script/fxmanifest.lua
+++ fivem_script/tokovoip_script/fxmanifest.lua
@@ -1,35 +1,37 @@
-fx_version "bodacious"
-games { "gta5", "rdr3" }
-rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+fx_version 'cerulean'
+games {'gta5', 'rdr3'}
 
-author "Itokoyamato, Plactrix & Neon"
-description "TokoVoIP V2: A simple FiveM VoIP script that uses TeamSpeak as the voice server"
-version "2.0.3"
-lua54 "yes"
+author 'Itokoyamato, Plactrix & Neon'
+description 'TokoVoIP V2: A simple FiveM VoIP script that uses TeamSpeak as the voice server'
+version '2.0.4'
+lua54 'yes'
 
 files {
-	"html/sounds/*.wav",
-	"html/index.html",
-	"html/script.js"
+    'html/sounds/*.wav',
+    'html/index.html',
+    'html/script.js'
 }
 
-ui_page "html/index.html"
-
-provide {
-	"mumble-voip",
-	"pma-voice"
-}
+ui_page 'html/index.html'
 
 shared_scripts {
-    "config.lua"
+    'config.lua',
+    '@ox_lib/init.lua'
 }
 
 client_scripts {
-    "client/cl_utils.lua",
-    "client/cl_main.lua"
+    'client/cl_utils.lua',
+    'client/cl_main.lua'
 }
 
 server_scripts {
-    "server/sv_utils.lua",
-    "server/sv_main.lua"
+    'server/sv_utils.lua',
+    'server/sv_main.lua'
+}
+
+dependency 'ox_lib'
+
+provides {
+    'mumble-voip',
+    'pma-voice'
 }
