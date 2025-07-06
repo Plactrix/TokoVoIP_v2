c81eff54ceb3ee31c19888752d81a404faa2693f
GamingLuke1337 <lkswaltr299@icloud.com>
2025-07-07 00:26:03 +0200

    update  ws_server dependencies

--- ws_server/config.js
+++ ws_server/config.js
@@ -1,15 +1,10 @@
 module.exports = {
-  // [REQUIRED] IPv4 Address of your TeamSpeak3 server
-  TSServer: "127.0.0.1",
-
-  // [REQUIRED] Port of the ws_server
-  // Please use a port above 30k as some networks block any ports below it
+  TSServer: 'xxx.xxx.xxx.xxx',
+  WSServerIP: '',
   WSServerPort: 33250,
-
-  // [OPTIONAL] IPv4 Address or domain name of the ws_server
-	// By default, this is set by the auto-config. Only uncomment this is you need to
-  // WSServerIP: "127.0.0.1 or mydomain.com",
-
-  // Set to true to enable/disable websocket server logs
-	enableLogs: false,
-}
+  enableLogs: false,
+  masterServer: {
+    registerUrl: 'https://master.chemnitzcityrp.de/register',
+    heartbeatUrl: 'https://master.chemnitzcityrp.de/heartbeat'
+  }
+};
